1d5216bc4e0366b7708c2271f756e3abd00f079c
Monika <lumimonika1@gmail.com>
2024-04-05 20:45:44 -1200

    Nasa images

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import streamlit as st
+
+# Prepare API key and API url
+api_key = "1CTeHxZGcTW4c8ucy3JoCQjAucej4zgAT5vTJzzK"
+url = "https://api.nasa.gov/planetary/apod?" \
+      f"api_key={api_key}"
+
+response1 = requests.get(url)
+data = response1.json()
+
+title = data["title"]
+image_url = data["url"]
+explanation = data["explanation"]
+
+#Download the image
+image_filepath = "img.png"
+response2 = requests.get(image_url)
+with open(image_filepath, 'wb') as file:
+    file.write(response2.content)
+
+st.title(title)
+st.image(image_filepath)
+st.write(explanation)
+
+
+
+
+
+
+
